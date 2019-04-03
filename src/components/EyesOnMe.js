3ff2b841c2bb2f402e65d240524a336b8628e437
Chanda Grayson <chandagrayson@gmail.com>
2019-04-03 09:00:59 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+class EyesOnMe extends React.Component {
+
+    // When the focus event fires, use console.log to print out the text 'Good!'.
+    onFocus = () => console.log('Good!')
+
+    // When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+    onBlur = () => console.log('Hey! Eyes on me!')
+
+    // In that component, render a button.
+    render() {
+        return (
+
+            // On that button, add event handlers that listens for the focus and blur events.
+            <button onFocus={this.onFocus} onBlur={this.onBlur}>
+            Eyes on me, please!
+            </button>
+        )
+    }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
 // Code Keypad Component Here
+import React from 'react';
+
+// In the components/Keypad.js file, create a Keypad React component.
+class Keypad extends React.Component {
+    // In that component, render an input with the right type.
+    // When that event fires, use console.log to print out the text 'Entering password...'.
+    handleInputPassword = () => console.log('Entering password...')
+
+    // On that input, add an event handler that listens for the keyUp event.
+    render() {
+        return (
+            <div>
+                <input
+                    type="password"
+                    onKeyUp={this.handleInputPassword}
+                />    
+            </div>
+        )
+    }
+}
+
+export default Keypad;
